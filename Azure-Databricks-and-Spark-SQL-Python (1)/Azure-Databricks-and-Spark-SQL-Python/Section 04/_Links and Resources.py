1c9c047acd1350e2ffffd508137d90da3818aed6
SushantGambhir <sushantgambhir9878@gmail.com>
2026-05-23 13:17:49 +0000

    File type

--- Azure-Databricks-and-Spark-SQL-Python (1)/Azure-Databricks-and-Spark-SQL-Python/Section 04/_Links and Resources.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC ### 🔗 Links and Resources
-# MAGIC
-# MAGIC - [Databricks Notebooks Overview](https://learn.microsoft.com/en-us/azure/databricks/notebooks/)
-# MAGIC - [Develop code in Databricks notebooks](https://learn.microsoft.com/en-us/azure/databricks/notebooks/notebooks-code#language-magic)
--- Azure-Databricks-and-Spark-SQL-Python (1)/Azure-Databricks-and-Spark-SQL-Python/Section 05/_Links and Resources.py
+++
@@ -1,5 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC ### 🔗 Links and Resources
-# MAGIC
-# MAGIC - [Databricks Compute Overview](https://learn.microsoft.com/en-us/azure/databricks/compute/)
